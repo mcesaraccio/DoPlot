70ca806f71a0a229e9d28daec608b769be14d398
Massimo Cesaraccio <massimo@streamago.com>
2017-11-12 17:20:35 +0100

    Initial Commit

---
+++ DoPlot/DoPlot.h
@@ -0,0 +1,19 @@
+//
+//  DoPlot.h
+//  DoPlot
+//
+//  Created by Massimo Cesaraccio on 12/11/2017.
+//  Copyright © 2017 Massimo Cesaraccio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DoPlot.
+FOUNDATION_EXPORT double DoPlotVersionNumber;
+
+//! Project version string for DoPlot.
+FOUNDATION_EXPORT const unsigned char DoPlotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoPlot/PublicHeader.h>
+
+
